8dff2dcb6072766805ee75a8556770f5a3af55d7
happyeveryday <fromdusktodawn@users.noreply.github.com>
2019-12-20 00:11:26 +0800

    test

--- c/test.c
+++ c/test.c
@@ -2,15 +2,13 @@
 	> File Name: test.c
 	> Author: Happyeveryday
 	> Mail: 2563546603@qq.com
-	> Created Time: 2019年12月14日 星期六 20时58分39秒
+	> Created Time: 2019年12月20日 星期五 00时11分00秒
  ************************************************************************/
 
 #include <stdio.h>
 #include <math.h>
 #include <inttypes.h>
 
-#define f(T, a) 
-
 int main() {
-    printf("%d", ({int a = 10; a - 5;}));
+    return 0;
 }
